fd4da461350dd2759568b76caf971a29ca7f8513
jeanjosephgeorge <jean.joseph@outlook.com>
2019-11-01 14:31:22 -0400

    small updates

--- src/main/java/com/revature/dao/BankDaoSQL.java
+++ src/main/java/com/revature/dao/BankDaoSQL.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.revature.model.Account;
 import com.revature.model.SubAccount;
 import com.revature.model.Transaction;
